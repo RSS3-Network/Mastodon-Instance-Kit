a10731f212f33972d0550c56e4dd5608c53a3bac
FrankLi123 <zcl@rss3.io>
2024-10-18 16:36:45 +0800

    feat: add rout hadnler for a new path

--- kafka_sender.py
+++ kafka_sender.py
@@ -14,12 +14,19 @@ producer = KafkaProducer(
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-@app.route('/inbox', methods=['POST'])
-def inbox():
-    data = request.get_json()
+def process_inbox(data):
     producer.send(KAFKA_TOPIC, data)
     return jsonify({'status': 'success'}), 202
 
+@app.route('/inbox', methods=['POST'])
+def inbox():
+    data = request.get_json()
+    return process_inbox(data)
+
+@app.route('/actor/inbox', methods=['POST'])
+def actor_inbox():
+    data = request.get_json()
+    return process_inbox(data)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3001)
-
